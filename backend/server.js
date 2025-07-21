215a6ab650a1e58c823246ee1ca22a7706cb8e39
divyeshx07 <divyeshapatel03@gmail.com>
2025-07-21 17:11:31 +0530

    Merge remote changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
-import app from "./app.js";
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('Some error occurred while connecting to database:', err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
+});
